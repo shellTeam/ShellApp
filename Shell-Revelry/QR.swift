a3ea383cc95dd265652c0ecc10a65800a4d199cb
johnmontewhat <us4john@gmail.com>
2016-09-08 22:50:32 -0700

    different api for QR

--- Shell-Revelry/QR.swift
+++ Shell-Revelry/QR.swift
@@ -10,15 +10,22 @@ import UIKit
 import AVFoundation
 
 protocol BarcodeDelegate {
-    
+    func barcodeReaded(barcode:String)
 }
 
-class QR: UIViewController, BarcodeDelegate {
+class QR: UIViewController, BarcodeDelegate, AVCaptureMetadataOutputObjectsDelegate {
+    @IBOutlet weak var lblQRCodeLabel: UILabel!
+    @IBOutlet weak var lblQRCodeResult: UILabel!
+    var objCaptureSession:AVCaptureSession?
+    var objCaptureVideoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var vwQRCode:UIView?
+  
+    
 
-    @IBOutlet weak var codeTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVideoCapture()
 
         // Do any additional setup after loading the view.
     }
@@ -30,13 +37,36 @@ class QR: UIViewController, BarcodeDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func barcodeIsReaded(barcode:String){
-            print("barcode: \(barcode)")
-        codeTextView.text = barcode
+//    func barcodeReaded(barcode:String){
+//            print("barcode: \(barcode)")
+//        codeTextView.text = barcode
+//    }
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        let barCodeViewController: QR = segue.destinationViewController as! QR
+//        barCodeViewController.delegate = self
+//    }
+    func configureVideoCapture() {
+        let objCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error:NSError?
+        let objCaptureDeviceInput: AnyObject!
+        do {
+            objCaptureDeviceInput = try AVCaptureDeviceInput(device: objCaptureDevice) as AVCaptureDeviceInput
+            
+        } catch let error1 as NSError {
+            error = error1
+            objCaptureDeviceInput = nil
+        }
+        if (error != nil) {
+            let alertView:UIAlertView = UIAlertView(title: "Device Error", message:"Device not Supported for this Application", delegate: nil, cancelButtonTitle: "Ok Done")
+            alertView.show()
+            return
+        }
+        objCaptureSession = AVCaptureSession()
+        objCaptureSession?.addInput(objCaptureDeviceInput as! AVCaptureInput)
+        let objCaptureMetadataOutput = AVCaptureMetadataOutput()
+        objCaptureSession?.addOutput(objCaptureMetadataOutput)
+        objCaptureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        objCaptureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
     }
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let barCodeViewController: QR = segue.destinationViewController as! QR
-        barCodeViewController.delegate = self
-    }
-
+   
 }
