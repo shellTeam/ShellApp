5ccf2b063ff860cbd715d596d384b36f7857c7fc
johnmontewhat <us4john@gmail.com>
2016-09-09 09:54:19 -0700

    update timer

--- Shell-Revelry/IntroScreen.swift
+++ Shell-Revelry/IntroScreen.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class IntroScreen: ViewController {
     
+    @IBOutlet weak var stopWatchLabel: UILabel!
     @IBOutlet var RevelLogo: UIImageView!
+    var startTime = NSTimeInterval()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,34 @@ class IntroScreen: ViewController {
         }
         
     }
+    func updateTime() {
+        
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        let strMinutes = String(format: "%02d", minutes)
+        
+        let strSeconds = String(format: "%02d", seconds)
+        
+        let strFraction = String(format: "%02d", fraction)
+        
+        stopWatchLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+          startTime = NSDate.timeIntervalSinceReferenceDate()
+        
+    }
+    
+
     
     
     
---
+++ Shell-Revelry/stopWatch.swift
@@ -0,0 +1,26 @@
+//
+//  stopWatch.swift
+//  Shell-Revelry
+//
+//  Created by John Montejano on 9/9/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import UIKit
+
+class stopWatch: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+}
