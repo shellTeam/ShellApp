1fa93b889f48c0160346ead672e68190eecf0b91
johnmontewhat <us4john@gmail.com>
2016-09-08 21:16:47 -0700

    Initial Commit

---
+++ Shell-RevelryTests/Shell_RevelryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shell_RevelryTests.swift
+//  Shell-RevelryTests
+//
+//  Created by John Montejano on 9/8/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import XCTest
+@testable import Shell_Revelry
+
+class Shell_RevelryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
