47a664d7be2eb195a8fe11dff4d0f7c85fd9939b
Jorge Bastos <jbastosneto@me.com>
2016-09-08 23:35:25 -0700

    added constant file

---
+++ Shell-Revelry/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Shell-Revelry
+//
+//  Created by Jorge Bastos on 9/8/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import Foundation
+
+let DEFAULT_USER_UID = "30"
